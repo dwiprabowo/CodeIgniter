4d762fac6bedc522e596dd28d8da9c4be79fb2ba
Dwi Prabowo <dwi.juli.prabowo@gmail.com>
2015-01-19 05:58:44 +0700

    auto_view: if cli return

--- application/hooks/auto_view.php
+++ application/hooks/auto_view.php
@@ -9,6 +9,9 @@ class AutoView{
     }
 
     function run(){
+        if($this->ci->input->is_cli_input()){
+            return;
+        }
         $template = $this->ci->template();
         $data = $this->ci->data();
         if($template){
