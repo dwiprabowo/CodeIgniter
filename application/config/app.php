56b7e2a4a742c85ee433a1b5a2a51ed1d17e41ea
Dwi Prabowo <dwi.juli.prabowo@gmail.com>
2015-01-20 08:58:21 +0700

    move app model value to config app

---
+++ application/config/app.php
@@ -0,0 +1,14 @@
+<?php
+
+define('APP_PREFIX', 'app_');
+
+$config[APP_PREFIX.'lang'] = "id";
+$config[APP_PREFIX.'viewport'] = [
+    'width' => 'device-width',
+    'initial-scale' => '1',
+];
+$config[APP_PREFIX.'x_ua_compatible_equiv'] = "IE=edge";
+$config[APP_PREFIX.'author'] = "dwi.juli.prabowo@gmail.com";
+$config[APP_PREFIX.'description'] = "description here...";
+$config[APP_PREFIX.'title'] = "App";
+$config[APP_PREFIX.'enable_profiler'] = FALSE;
--- application/models/base/App_model.php
+++ application/models/base/App_model.php
@@ -2,23 +2,20 @@
 
 class App_Model extends MY_Model{
 
-    private $lang = "id";
-    private $viewport = [
-        'width' => 'device-width',
-        'initial-scale' => '1',
-    ];
-    private $x_ua_compatible_equiv = "IE=edge";
-    private $author = "dwi.juli.prabowo@gmail.com";
-    private $description = "description here...";
-    private $title = "App";
-    private $enable_profiler = FALSE;
-
     function __construct(){
         parent::__construct();
+        $this->load->config('app');
         $this->build();
     }
 
     private function build(){
+        $this->lang = config_item(APP_PREFIX."lang");
+        $this->x_ua_compatible_equiv = config_item(APP_PREFIX."x_ua_compatible_equiv");
+        $this->author = config_item(APP_PREFIX."author");
+        $this->description = config_item(APP_PREFIX."description");
+        $this->title = config_item(APP_PREFIX."title");
+        $this->enable_profiler = config_item(APP_PREFIX."enable_profiler");
+        $this->viewport = config_item(APP_PREFIX."viewport");
         $this->viewport = implode(
             ', '
             , array_map(
