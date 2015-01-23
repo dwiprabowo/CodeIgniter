8215c575c487003848fcb748ca4ff66c9b256b32
Dwi Prabowo <dwi.juli.prabowo@gmail.com>
2015-01-23 08:04:38 +0700

    update home page

--- application/views/home/index.php
+++ application/views/home/index.php
@@ -1,11 +1,24 @@
 <style>
+    div.home-index-page-wrapper{
+        margin-top: 10%;
+    }
     img.my-photo{
         width: 128px;
         border-radius: 128px;
         border: 8px solid rgba(0, 0, 255, .2);
         padding: 8px;
     }
+    div.button-wrapper{
+        margin-top: 2%;
+    }
 </style>
-<div class="text-center">
-    <?=assets('dwi.png', ['class' => 'my-photo'])?>
+<div class="text-center home-index-page-wrapper">
+    <div class="animation-wrapper">
+        <?=assets('dwi.png', ['class' => 'my-photo'])?>
+    </div>
+    <div class="button-wrapper">
+        <button class="btn btn-primary btn-lg">
+            Let me say something?
+        </button>
+    </div>
 </div>
