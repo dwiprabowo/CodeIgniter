49c0616290066d052d9054583781e1cea80397a2
Dwi Prabowo <dwi.juli.prabowo@gmail.com>
2015-01-28 07:26:37 +0700

    me start talking

--- application/config/app.php
+++ application/config/app.php
@@ -10,6 +10,6 @@ $config[APP_PREFIX.'viewport'] = [
 $config[APP_PREFIX.'x_ua_compatible_equiv'] = "IE=edge";
 $config[APP_PREFIX.'author'] = "dwi.juli.prabowo@gmail.com";
 $config[APP_PREFIX.'description'] = "description here...";
-$config[APP_PREFIX.'title'] = "App";
-$config[APP_PREFIX.'title_html'] = "<strong>App</strong>";
+$config[APP_PREFIX.'title'] = "Profil Online";
+$config[APP_PREFIX.'title_html'] = "<strong>Profil</strong> <em>Online</em>";
 $config[APP_PREFIX.'enable_profiler'] = FALSE;
--- application/views/home/index.php
+++ application/views/home/index.php
@@ -2,23 +2,102 @@
     div.home-index-page-wrapper{
         margin-top: 10%;
     }
-    img.my-photo{
-        width: 128px;
-        border-radius: 128px;
-        border: 8px solid rgba(0, 0, 255, .2);
-        padding: 8px;
+    div.screen-wrapper{
+        display: inline-block;
+        border: 1px solid black;
+        padding: 0;
+        margin: 0;
+        height: 130px;
     }
     div.button-wrapper{
         margin-top: 2%;
     }
+    div.animated_image_place_wrapper{
+        display: inline-block;
+        width: 128px;
+        height: 128px;
+        position: relative;
+    }
+    img.image-frames{
+        width: 128px;
+        height: 128px;
+        position: absolute;
+        top: 0;
+        left: 0;
+    }
 </style>
 <div class="text-center home-index-page-wrapper">
-    <div class="animation-wrapper">
-        <?=assets('dwi.png', ['class' => 'my-photo'])?>
+    <div class="text-center">
+        <div 
+            id="popup_text" 
+            class="screen-wrapper"
+        >
+            <div class="animated_image_place_wrapper">
+                <img class="image-frames" src="<?=base_url('assets/img/face-animated/base.png')?>">
+                <img id="eyes" class="image-frames" src="<?=base_url('assets/img/face-animated/eyes-opened.png')?>">
+            </div>
+        </div>
     </div>
     <div class="button-wrapper">
-        <button class="btn btn-primary btn-lg">
-            Let me say something?
-        </button>
+        <button 
+            type="button" 
+            id="speak_trigger"
+            class="btn btn-lg btn-danger" 
+            title="Popover title" 
+            onclick="$('#trigger').popover('toggle')" 
+        ><span class="glyphicon glyphicon-play"></span></button>
     </div>
 </div>
+
+<script>
+    $(function(){
+        var message = [
+            {content: "Halo!", time: 2000},
+            {content: "Perkenalkan", time: 1000},
+            {content: "Nama saya <strong>Juli</strong>", time: 3000},
+            {content: "bekerja sebagai <strong>Web Programmer</strong>", time: 4000},
+            {content: "memiliki pengalaman kerja", time: 1500},
+            {content: "kurang lebih selama <strong>3 Tahun</strong>", time: 3000},
+        ];
+        var base_path = "<?=base_url()?>";
+        var eyes = [
+            'assets/img/face-animated/eyes-closed.png',
+            'assets/img/face-animated/eyes-opened.png',
+        ];
+        setInterval(blink, 4000);
+        function blink(){
+            $("#eyes").attr('src', base_path+eyes[0]);
+            setTimeout(function(){
+                $("#eyes").attr('src', base_path+eyes[1]);
+            }, (1000*Math.random()));
+        }
+
+        $("#speak_trigger").click(function(){
+            speak();
+        });
+
+        function speak(){
+            var time_padding = 0;
+            for(var i = 0;i < message.length;i++){
+                console.log(time_padding);
+                updateContent(message[i].content, message[i].time, time_padding);
+                time_padding += message[i].time;
+                time_padding += 1500;
+            }
+        }
+
+        function updateContent(message, time, time_start){
+            setTimeout(function(){
+                console.log('called: '+message);
+                $("#popup_text").popover({
+                    html: true,
+                    content: message,
+                });
+                $("#popup_text").popover('show');
+                setTimeout(function(){
+                    $("#popup_text").popover('destroy');
+                }, time+1000);
+            }, time_start);
+        }
+    });
+</script>
